5f65550a4be0d8b34a16afa4e462c1cb2112aa04
Chris <133912058+ch627110@users.noreply.github.com>
2025-07-30 22:00:05 -0400

    Add event listeners to buttons

--- script.js
+++ script.js
@@ -27,3 +27,68 @@ function operate(operator, number1, number2) {
         return divide(number1, number2)
     }
 }
+
+const container = document.getElementById('container')
+
+const numberGrid = document.getElementById('one-nine')
+for(let i = 1; i <= 9; i++) {
+    const button = document.createElement('button')
+    
+    button.textContent = i;
+    
+    button.addEventListener('click', () => {
+        console.log(button.textContent)
+    });
+
+    numberGrid.appendChild(button)
+}
+
+const zeroRow = document.getElementById('zero')
+for(let i = 10; i < 13; i++) {
+    const button = document.createElement('button')
+    if(i == 10) {
+        button.textContent = 0;
+    } else if (i == 11) {
+        button.textContent = '.';
+    } else {
+        button.textContent = 'C';
+    }
+    button.addEventListener('click', () => {
+        buttonClick(button)
+    });
+    zeroRow.appendChild(button)
+}
+
+const operators = document.getElementById('operators')
+for(let i = 13; i < 17; i++) {
+    const button = document.createElement('button')
+    if(i == 13) {
+        button.textContent = '+';
+    } else if (i == 14) {
+        button.textContent = '—';
+    } else if (i == 15) {
+        button.textContent = '×';
+    } else {
+        button.textContent = '/';
+    }
+    button.addEventListener('click', () => {
+        buttonClick(button)
+    });
+    operators.appendChild(button)
+}
+
+function buttonClick(button) {
+    if(button.textContent == '+') {
+
+    } else if(button.textContent == '—') {
+        
+    } else if(button.textContent == '×') {
+        
+    } else if(button.textContent == '/') {
+        
+    } else if(button.textContent == '=') {
+        
+    } else {
+
+    }
+}
